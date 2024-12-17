27fac3d920c7f848f2f630a79440e6212652c8cb
Ebenezer-ebu <eifezulike@gmail.com>
2024-12-17 01:02:34 +0100

    added sidenav

---
+++ src/hooks/useIcons.tsx
@@ -0,0 +1,24 @@
+import shrink from '../assets/burger.svg';
+import logo from '../assets/logo.svg';
+import home from '../assets/home.svg';
+import homei from '../assets/homei.svg';
+import cart from '../assets/cart.svg';
+import carti from '../assets/carti.svg';
+import setting from '../assets/setting.svg';
+import settingi from '../assets/settingi.svg';
+import logout from '../assets/logout.svg';
+
+export const useIcons = () => {
+  return {
+    // slide bar icons
+    shrink,
+    logo,
+    home,
+    homei,
+    cart,
+    carti,
+    setting,
+    settingi,
+    logout,
+  };
+};
