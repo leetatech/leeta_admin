415f2fdc2dc8e678aa46394507ffab827a226367
Ebenezer-ebu <eifezulike@gmail.com>
2024-12-19 23:41:49 +0100

    removed used variables

--- src/view/components/header/Header.tsx
+++ src/view/components/header/Header.tsx
@@ -1,24 +1,11 @@
-/* eslint-disable @typescript-eslint/comma-dangle */
-/* eslint-disable @typescript-eslint/semi */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/prefer-optional-chain */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-/* eslint-disable array-callback-return */
-/* eslint-disable multiline-ternary */
-/* eslint-disable @typescript-eslint/member-delimiter-style */
-/* eslint-disable @typescript-eslint/space-before-function-paren */
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 import { useIcons } from '../../../hooks/useIcons';
 
 import { type RootState } from '../../../state';
-
 import './Header.css';
-// interface IProps {
-//   burgerClick: () => void;
-// }
 
 interface IFilter {
   title: string;
@@ -40,10 +27,7 @@ interface IProps {
 export default function Header({ shrinked, setShrinked, burgerClick, headTag }: IProps) {
   const { logo, shrink } = useIcons();
   const { auth } = useSelector((state: RootState) => state);
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const dispatch = useDispatch<any>();
-  const [searchTerm, setSearchTerm] = useState('');
+
   const [dropValue, setDropValue] = useState<IFilter>({
     title: '',
     value: '',
@@ -69,16 +53,6 @@ export default function Header({ shrinked, setShrinked, burgerClick, headTag }: 
             <img className='w-full h-full object-cover' src={shrink} alt='icon' />
           </div>
         )}
-
-        {/* <div className='left-hand'>
-          <div className='menu-box burger-icon-color' onClick={burgerClick}>
-            <img src={menu} alt='menu' />
-          </div>
-          <div className='head-tag'>
-            <Typography>{headTag}</Typography>
-          </div>
-        </div> */}
-
         <div className='user-utils'></div>
       </div>
     </>
