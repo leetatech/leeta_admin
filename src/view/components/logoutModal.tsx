f2167e946a3fb06aff55fad4667a227f5c118c1e
Ebenezer-ebu <eifezulike@gmail.com>
2025-01-21 17:16:52 +0100

    changes to settings page and added a modal component

--- src/view/components/logoutModal.tsx
+++ src/view/components/logoutModal.tsx
@@ -1,41 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const LogoutModal = ({ 
-  isOpen, 
-  onClose, 
-  onLogout 
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  onLogout: () => void;
-}) => {
-  if (!isOpen) return null;
+const LogoutModal = ({ onClose, onLogout }: { onClose: () => void; onLogout: () => void }) => {
+  // if (!isOpen) return null;
 
   return (
-    <div className="fixed w-full h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      {/* Modal */}
-      <div className="bg-white rounded-t h-44 p-6 w-96 shadow-t-lg">
-        <div className="flex justify-center flex-col mb-6">
-          <h2 className="text-xl font-semibold mb-2">Log Out</h2>
-          <p className="text-gray-600">Are you sure you want to log out?</p>
-        </div>
-        <div className="flex flex-col gap-3 justify-center items-center">
-          <button 
-            onClick={onClose}
-            className="border border-gray-300 rounded-md w-full py-2 hover:bg-gray-50 transition-colors"
-          >
-            Cancel
-          </button>
-          <button 
-            onClick={onLogout}
-            className="bg-[#DC0121] text-white rounded-md w-full py-2 hover:bg-[#b30018] transition-colors"
-          >
-            Log out
-          </button>
-        </div>
+    <div className='bg-white rounded-t p-6 w-96 shadow-t-lg'>
+      <div className='flex justify-center flex-col mb-6'>
+        <h2 className='text-xl font-semibold mb-2'>Log Out</h2>
+        <p className='text-gray-600'>Are you sure you want to log out?</p>
+      </div>
+      <div className='flex flex-col gap-3 justify-center items-center'>
+        <button onClick={onClose} className='border border-gray-300 rounded-md w-full py-2 hover:bg-gray-50 transition-colors'>
+          Cancel
+        </button>
+        <button onClick={onLogout} className='bg-[#DC0121] text-white rounded-md w-full py-2 hover:bg-[#b30018] transition-colors'>
+          Log out
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogoutModal
+export default LogoutModal;
--- src/view/pages/Settings.tsx
+++ src/view/pages/Settings.tsx
@@ -1,72 +1,45 @@
-import React, { useState } from "react";
-import Typography from "../components/Typography/Typography";
-import { TypographyVariant } from "../components/types";
-import UPLOAD from "../../assets/uplooad.svg";
-import { TbLogout } from "react-icons/tb";
+import React, { useState } from 'react';
+import Typography from '../components/Typography/Typography';
+import { TypographyVariant } from '../components/types';
+import UPLOAD from '../../assets/uplooad.svg';
+import { TbLogout } from 'react-icons/tb';
+import Modal from '../components/Modal';
+import LogoutModal from '../components/logoutModal';
 
 const Settings = () => {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   const handleLogout = () => {
     window.location.href = '/';
-    console.log("Logging out...");
+    console.log('Logging out...');
   };
 
   return (
-    <div className="px-6 py-4">
+    <div className='px-6 py-4'>
       <Typography variant={TypographyVariant.TITLE}>Settings</Typography>
-      <section className="flex flex-col justify-center items-center pt-24 w-full">
-        <img src={UPLOAD} alt="Coming soon" />
-        <div className="text-center w-full max-w-md">
-          <Typography variant={TypographyVariant.BODY_DEFAULT_MEDIUM}>
-            Daniel Tesfaye
-          </Typography>
-          <Typography variant={TypographyVariant.SMALL}>
-            Abeltesfaye@Gmail.com
-          </Typography>
-          <div 
-            className="bg-[#FFFFFF] py-4 flex items-center px-3 mt-3 w-full max-w-sm mx-auto"
-            onClick={() => setShowLogoutModal(true)}
-          >
-            <TbLogout fontWeight={700} color="#C11F1F" />
-            <Typography
-              variant={TypographyVariant.BODY_DEFAULT_MEDIUM}
-              className="text-red-700 text-start px-2 cursor-pointer"
-            >
+      <section className='flex flex-col justify-center items-center pt-24 w-full'>
+        <img src={UPLOAD} alt='Coming soon' />
+        <div className='text-center w-full max-w-md'>
+          <Typography variant={TypographyVariant.BODY_DEFAULT_MEDIUM}>Daniel Tesfaye</Typography>
+          <Typography variant={TypographyVariant.SMALL}>Abeltesfaye@Gmail.com</Typography>
+          <div className='bg-[#FFFFFF] py-4 flex items-center px-3 mt-3 w-full max-w-sm mx-auto' onClick={() => setShowLogoutModal(true)}>
+            <TbLogout fontWeight={700} color='#C11F1F' />
+            <Typography variant={TypographyVariant.BODY_DEFAULT_MEDIUM} className='text-red-700 text-start px-2 cursor-pointer'>
               Log out
             </Typography>
           </div>
         </div>
       </section>
 
-      {showLogoutModal && (
-        <div className="fixed w-full h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        {/* Modal */}
-        <div className="bg-white rounded-t-lg h-[249px] font-title  p-6 w-[375px] shadow-t-lg">
-          <div className="flex justify-center items-center flex-col mb-6">
-            <h2 className="text-[22px] font-semibold mb-2">Log Out</h2>
-            <p className="text-gray-600 text-[11px] font-title font-normal">Are you sure you want to log out?</p>
-          </div>
-          <div className="flex flex-col gap-3 justify-center items-center">
-            <button 
-              onClick={() => setShowLogoutModal(false)}
-              className="border border-gray-300 text-[15px] font-title rounded-md w-[343px] h-[44px] py-2 hover:bg-gray-50 transition-colors"
-            >
-              Cancel
-            </button>
-            <button 
-              onClick={() => {
-                        handleLogout();
-                       window.location.href = '/';
-                         }}
-              className="bg-[#DC0121] text-[15px] font-title text-white rounded-md w-[343px] h-[44px] py-2 hover:bg-[#b30018] transition-colors"
-            >
-              Log out
-            </button>
-          </div>
-        </div>
-      </div>
-      )}
+      <Modal isOpen={showLogoutModal}>
+        <LogoutModal
+          onClose={() => setShowLogoutModal(false)}
+          onLogout={() => {
+            handleLogout();
+            window.location.href = '/';
+          }}
+        />
+      </Modal>
     </div>
   );
 };
